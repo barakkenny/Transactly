6fca308f5dee4f18beac0f577c8c92680bdda3de
Kehinde Yusuf <95845686+barakkenny@users.noreply.github.com>
2025-10-26 12:54:18 +0100

    Added add to, remove from cart functionality

--- src/components/menu/CartInfo.jsx
+++ src/components/menu/CartInfo.jsx
@@ -1,23 +1,51 @@
 import { FaNotesMedical, FaUserCircle } from "react-icons/fa"
 import {RiDeleteBin2Fill } from "react-icons/ri"
+import { useDispatch, useSelector } from "react-redux";
+import { removeItems } from "../../redux/slices/cartSlice";
+import { useEffect, useRef } from "react";
 
 const CartInfo = () => {
+  const dispatch = useDispatch()
+  const scrollRef = useRef()
+
+  const cartData = useSelector((state) => state.cart)
+
+  const removeItemInCart = (itemId) => {
+    dispatch(removeItems(itemId))  
+  }
+
+  useEffect(() => {
+    if(scrollRef.current){
+      scrollRef.current.scrollTo({
+        top: scrollRef.current.scrollHeight,
+        behavior: "smooth"
+      })
+
+    }
+  })
+
   return (
     <div className="px-4 py-2">
       <h1 className="text-lg text-[#e4e4e4] font-semibold tracking-wide">
         Order Details
       </h1>
-      <div className="mt-4 overflow-y-scroll scrollbar-hide h-[380px]">
-        <div className="bg-[#1f1f1f] rounded-lg px-4 py-4 mb-2">
+      <div className="mt-4 overflow-y-scroll scrollbar-hide h-[380px]" ref={scrollRef}>
+ 
+          {cartData.length === 0 ? (
+            <p className="h-[380px] text-[#ababab] text-sm flex justify-center items-center">Your cart is empty</p>
+          ) : (
+            cartData.map((item) => (
+              <div key={item.id} className="bg-[#1f1f1f] rounded-lg px-4 py-4 mb-2">
           <div className="flex items-center justify-between">
             <h1 className="text-[#ababab] font-semibold tracking-wide text-md">
-              Chicken Tikka
+              {item.name}
             </h1>
-            <p className="text-[#ababab] font-semibold">x2</p>
+            <p className="text-[#ababab] font-semibold">x{item.quantity}</p>
           </div>
           <div className="flex items-center justify-between mt-3">
             <div className="flex items-center gap-3">
               <RiDeleteBin2Fill
+              onClick={()=> removeItemInCart(item.id)}
                 className="text-[#ababab] cursor-pointer"
                 size={20}
               />
@@ -26,51 +54,10 @@ const CartInfo = () => {
                 size={20}
               />
             </div>
-            <p className="text-[#f5f5f5] text-md font-bold">$123</p>
+            <p className="text-[#f5f5f5] text-md font-bold">${item.price}</p>
           </div>
         </div>
-        <div className="bg-[#1f1f1f] rounded-lg px-4 py-4 mb-2">
-          <div className="flex items-center justify-between">
-            <h1 className="text-[#ababab] font-semibold tracking-wide text-md">
-              Chicken Tikka
-            </h1>
-            <p className="text-[#ababab] font-semibold">x2</p>
-          </div>
-          <div className="flex items-center justify-between mt-3">
-            <div className="flex items-center gap-3">
-              <RiDeleteBin2Fill
-                className="text-[#ababab] cursor-pointer"
-                size={20}
-              />
-              <FaNotesMedical
-                className="text-[#bababa] cursor-pointer"
-                size={20}
-              />
-            </div>
-            <p className="text-[#f5f5f5] text-md font-bold">$123</p>
-          </div>
-        </div>
-        <div className="bg-[#1f1f1f] rounded-lg px-4 py-4 mb-2">
-          <div className="flex items-center justify-between">
-            <h1 className="text-[#ababab] font-semibold tracking-wide text-md">
-              Chicken Tikka
-            </h1>
-            <p className="text-[#ababab] font-semibold">x2</p>
-          </div>
-          <div className="flex items-center justify-between mt-3">
-            <div className="flex items-center gap-3">
-              <RiDeleteBin2Fill
-                className="text-[#ababab] cursor-pointer"
-                size={20}
-              />
-              <FaNotesMedical
-                className="text-[#bababa] cursor-pointer"
-                size={20}
-              />
-            </div>
-            <p className="text-[#f5f5f5] text-md font-bold">$123</p>
-          </div>
-        </div>
+          )))}
       </div>
     </div>
   );
